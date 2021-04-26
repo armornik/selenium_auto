8e945103bf65c8bd3a02a8d074999962cca7fcbb
armornik <kd200681@mail.ru>
2021-04-26 17:02:01 +0300

    radiobutton and checkbox

---
+++ selenium_course/part2/radiobutton.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+import time
+import math
+
+
+def calc(integer_from_site: str) -> str:
+    return str(math.log(abs(12*math.sin(int(integer_from_site)))))
+
+
+link = "http://suninjuly.github.io/math.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+
+    option1 = browser.find_element_by_id("robotCheckbox")
+    option1.click()
+
+    option2 = browser.find_element_by_id("robotsRule")
+    option2.click()
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 20 секунд
+    time.sleep(20)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
